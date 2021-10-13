4c5a83334bb6921ded637d2d9ebbb396aea4050c
Filipe Medeiros <filipecmedeiros88@gmail.com>
2021-10-13 03:41:51 -0300

    PNS 2013

---
+++ PNS_Script_2013.R
@@ -0,0 +1,19 @@
+#install.packages('PNSIBGE')
+
+library(PNSIBGE)
+
+df_tibble <- read_pns('data/PNS/2013/Microdados/Dados/PNS_2013.txt', 'data/PNS/2013/Microdados/Documentação/Dicionario_e_input_20200930/input_PNS_2013.txt')
+
+df_tibble
+
+length(df_tibble)
+
+class(df_tibble)
+
+write.csv(df_tibble, 'data/extract/df_tibble_2013.csv')
+
+df <- as.data.frame(df_tibble)
+
+df
+
+write.csv(df, 'data/extract/df_2013.csv')
